8f0e8e1e5cdbf9ab9d9dad80905786a06f1eca9e
sarran528 <sarranm528@gmail.com>
2025-07-25 14:36:05 +0530

    system setup, i/o

---
+++ src/basic.java
@@ -0,0 +1,7 @@
+public class basic {
+    public static void main(String []args){
+        System.out.print("Hello World");
+        System.out.print(" from Java!\n");
+        System.out.println("This is a basic Java program.");
+    }
+}
