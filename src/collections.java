1ffdd9726d4e0355ed4b7c2f6bb503abe24740c7
sarran528 <sarranm528@gmail.com>
2025-08-04 23:15:50 +0530

    inheritance

---
+++ src/collections.java
@@ -0,0 +1,13 @@
+public class collections {
+
+    public static void main(String [] args){
+
+//        String[] name =new String [5];
+
+        String [] name = {"zoro","zoro","zoro","zoro","zoro"};
+
+        System.out.print(name[2]);
+
+    }
+
+}
---
+++ src/oops.java
@@ -0,0 +1,36 @@
+public class oops {
+
+    public static void main(String[] args) {
+
+        //inheritance - attributes and method from one class to another
+        // extends keyword in used to pass the parent methods to the child
+
+        children sarran = new children();
+        System.out.println(sarran.age);
+        System.out.println(sarran.nationality);
+        sarran.food();
+    }
+
+
+    static class grandparent {
+
+        String nationality ;
+
+    }
+
+    static class parent extends grandparent {
+//       String nationality = "indian";
+
+        void food() {
+
+            System.out.println("Non vegetarian");
+
+        }
+
+    }
+
+    static class children extends parent {
+
+        int age = 30;
+    }
+}
