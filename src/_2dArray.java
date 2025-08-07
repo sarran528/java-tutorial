c11194dd21ed48706593a60abae2a0b9f0b7bf70
sarran528 <sarranm528@gmail.com>
2025-08-07 23:55:00 +0530

    2d print

--- src/_2dArray.java
+++ src/_2dArray.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class _2dArray {
+static Scanner sc = new Scanner(System.in);
+    public static void main(String []args){
+
+
+        //2d array input
+        System.out.println("Enter the no of columns : ");
+        int m = sc.nextInt();
+        System.out.println("Enter the no of rows : ");
+        int n = sc.nextInt();
+
+        int[][] arr = new int[m][n];
+
+
+        //2d array output
+        for(int i=0;i<m;i++){
+
+        }
+
+    }
 }
