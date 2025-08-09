83348cf59637d1d457972409b00c5f5dd2ad5a0a
sarran528 <sarranm528@gmail.com>
2025-08-09 23:04:52 +0530

    output for 2d array

--- src/_2dArray.java
+++ src/_2dArray.java
@@ -7,9 +7,10 @@ public class _2dArray {
 
 
         //2d array input
-        System.out.print("Enter the no of columns : ");
+        System.out.print("Enter the no of rows: ");
         int m = sc.nextInt();
-        System.out.print("Enter the no of rows : ");
+        System.out.print("Enter the no of columns4" +
+                " : ");
         int n = sc.nextInt();
 
         int[][] arr = new int[m][n];
@@ -18,14 +19,30 @@ public class _2dArray {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
-                System.out.printf("Enter the value for array element %d %d :",i+1,j+1);
-                arr[i][j]=sc.nextInt();
+                System.out.printf("Enter the value for array element %d %d :", i + 1, j + 1);
+                arr[i][j] = sc.nextInt();
 
-        //2d array output
             }
+        }
+        //2d array output
 
+        System.out.printf("%dx%d Matrix is:\n",m,n);
+
+//        System.out.print("{");
+        for (int i = 0; i < m; i++) {
+            System.out.print("|");
+            for (int j = 0; j < n; j++) {
+
+                System.out.print(arr[i][j]);
+                if (j!= n-1) {
+                    System.out.print(",");
+
+                }
+            }System.out.print("|");
+            if (i != m-1){
+            System.out.print("\n");}
 
         }
-
+//        System.out.print("}");
     }
 }
