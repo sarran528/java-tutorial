facdfef02c5af025c1e76a53d42c2fc3514be33e
sarran528 <sarranm528@gmail.com>
2025-08-09 22:42:30 +0530

    practise programs IN COLLECTIONS

--- src/_2dArray.java
+++ src/_2dArray.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 
 public class _2dArray {
-static Scanner sc = new Scanner(System.in);
-    public static void main(String []args){
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
 
 
         //2d array input
-        System.out.println("Enter the no of columns : ");
+        System.out.print("Enter the no of columns : ");
         int m = sc.nextInt();
-        System.out.println("Enter the no of rows : ");
+        System.out.print("Enter the no of rows : ");
         int n = sc.nextInt();
 
         int[][] arr = new int[m][n];
 
 
-        //2d array output
-        for(int i=0;i<m;i++){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
+                System.out.printf("Enter the value for array element %d %d :",i+1,j+1);
+                arr[i][j]=sc.nextInt();
+
+        //2d array output
+            }
 
 
         }
