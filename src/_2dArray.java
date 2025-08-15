a5584e67c109436a8f048fb8c10f9eebe456b126
sarran528 <sarranm528@gmail.com>
2025-08-15 23:42:31 +0530

    jagged array

--- src/_2dArray.java
+++ src/_2dArray.java
@@ -47,6 +47,13 @@ public class _2dArray {
             System.out.print("\n");}
 
         }
+
+//        int[][] jagged = {
+//                {1, 2, 3},
+//                {4, 5},
+//                {6, 7, 8, 9}
+//        };
+
 //        System.out.print("}");
     }
 
