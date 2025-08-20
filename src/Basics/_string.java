929661470b136960a9dedc9f7be4f495ad17c234
sarran528 <sarranm528@gmail.com>
2025-08-20 23:55:57 +0530

    main for collections added

--- src/Basics/_string.java
+++ src/Basics/_string.java
@@ -1,4 +1,20 @@
 package Basics;
 
 public class _string {
+
+    public abstract class Functions{
+        add();
+    }
+
+
+    public static void main(String[] args) {
+        String a = "sarran";
+
+        System.out.print(a);
+
+        char[] b = new char[]{'a', 'b', 'c'};
+        System.out.print(b);
+    }
 }
+
+
