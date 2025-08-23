ce51c451fc4e4b3ed4b2e816c087a4ee55c907bb
sarran528 <sarranm528@gmail.com>
2025-08-23 22:55:05 +0530

    Linked list

--- src/Basics/_string.java
+++ src/Basics/_string.java
@@ -2,18 +2,18 @@ package Basics;
 
 public class _string {
 
-    public abstract class Functions{
-        add();
-    }
+
 
 
     public static void main(String[] args) {
-        String a = "sarran";
+//        System.out.print(a);
 
-        System.out.print(a);
-
+        String x="R";
+        String y="r";
         char[] b = new char[]{'a', 'b', 'c'};
-        System.out.print(b);
+        String a = new String(b);
+//        System.out.print(a);
+        System.out.print(y.compareTo(x));
     }
 }
 
