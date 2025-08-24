1f07da5bf1b203283fb2d051753dd450c313f291
sarran528 <sarranm528@gmail.com>
2025-08-24 23:48:39 +0530

    Array list

---
+++ src/Collections/homework.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.*;
+
+public class homework {
+    public static void main(String[] args) {
+        q1();
+    }
+
+
+    static void q1() {
+        ArrayList<Integer> List = new ArrayList<>();
+
+        for (int i = 0; i <= 5; i++) {
+             List.add(i);
+        }
+        System.out.println(List);
+    }
+}
